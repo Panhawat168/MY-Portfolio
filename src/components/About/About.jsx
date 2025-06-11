c11d00b4c796c8e10b978eff9ca40117d47d029b
Panhwat2023 <menphanhawat@gmail.com>
2025-06-11 13:12:20 +0700

    done section skills

---
+++ src/components/About/About.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { TypeAnimation } from 'react-type-animation';
+import profileImage from '../../assets/Wat.jpg';
+import Tilt from 'react-parallax-tilt';
+
+
+const About = () => {
+  return (
+    <section id='about' className='py-4 px-[7vw] md:px-[7vw] lg:px-[20vw] font-sans mt-16 md:mt-24 lg:mt-42'>
+      <div className=' flex  flex-col-reverse md:flex-row justify-between items-center'>
+        {/* Left side */}
+
+        <div className='md:w-1/2 text-center  md:text-left mt-8 md:mt-0'>
+          {/* Welcome */}
+          <h1 className='text-3xl sm:text-5xl md:text-6xl font-bold text-white mb-2 leading-2'>
+            Hi,I am
+          </h1>
+          {/* Name */}
+          <h1 className='text-4xl sm:text-5xl md:text-6xl font-bold text-white'>
+            Men Panhawat
+          </h1>
+          {/* Skill heading Effect */}
+          <h3 className='text-xl sm:text-2xl md:text-3xl font-semibold mb-4 text-[#8245ec] leading-tight'>
+            <span className=' text-white'>
+              I am
+            </span>
+            <TypeAnimation
+              sequence={[
+                'Fullstack Developer', 2000, // Display text, wait 2000ms
+                'Coder', 2000,
+              ]}
+              speed={50} // Maps to 100 in react-typing-effect (inverted scale)
+              deletionSpeed={75} // Maps to eraseSpeed=50
+              cursor={true}
+              repeat={Infinity}
+              style={{ color: '#0d83fd' }} // Replaces cursorRenderer
+            />
+          </h3>
+          {/* about me paragraph */}
+          <p className=' text-base text-justify sm:text-lg md:text-lg text-gray-400 mb-10 mt-8 leading-relaxed sm:tracking-normal'>
+            I am seeking a Web Designer position to apply my passion for innovation and
+            problem-solving,along with my real-world experience in building
+            full-stack web applications and managing IT systems.
+            I have worked with both front-end and back-end technologies, enabling me to create websites that are not only visually appealing but also function seamlessly. Through this opportunity, I aim to leverage my skills and experience to contribute meaningfully to the technological advancements within your community.
+          </p>
+          {/* my cv */}
+
+          <a href="/src/assets/PanhawatCv.v1.pdf" target="_blank" rel="noopener noreferrer" className=' inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105'
+            style={{
+              background: ' linear-gradient(90deg,#0d83fd,#a855f7)',
+              boxShadow: '0 0 2px ##0d83fd, 0 0 2px #0d83fd, 0 0 40px #0d83fd '
+            }}>
+
+            Download Resume
+          </a>
+        </div>
+        {/* Right side Imge pf */}
+        <div className='md:w-1/2 flex justify-center md:justify-end'>
+
+
+
+          <img className='w-50 h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(0_10px_20px_rgb(130,69,236,0.5))]' src={profileImage} alt="" srcset="" />
+
+        </div>
+
+      </div>
+    </section>
+  );
+};
+
+export default About;
